f7f0a73544b49930ca1abcf515852fa1409f00f9
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2022-01-26 12:16:36 -0500

    Create 0126HelloWorldMethod.java

---
+++ 0126HelloWorldMethod.java
@@ -0,0 +1,30 @@
+// Carrie Krueger
+// 8/23/2021
+// This program writes a message to the screen.
+// *Note: do this in your original HelloWorld.java file
+
+
+public class HelloWorldMethod {
+    public static void main(String[] args) {
+        printMessage();  // calls a method that prints a message to the screen
+        
+    }
+    
+    //this method prints a message to the screen
+    public static void printMessage() {
+        System.out.println("Hello, World!"); //writes to the screen
+    }
+}
+ 
+// 1. write comment and public static void below main method
+// 2. copy print statement from main method to new method 
+// 3. call the new method in the main method
+
+// Fill these in!
+/*
+Where did you struggle with this coding work?
+
+What was easy?
+
+What questions do you still have?
+*/
