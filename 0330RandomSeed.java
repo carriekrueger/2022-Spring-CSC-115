da2069c71489df37b36a6b204dcaa74cb0657f4c
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2022-03-30 10:59:56 -0400

    Update 0330RandomSeed.java

--- 0330RandomSeed.java
+++ 0330RandomSeed.java
@@ -27,9 +27,24 @@ public class RandomSeed {
         
         int rand1 = rand.nextInt(100);  // between 0 - 99
         System.out.println("Random number 1 = " + rand1);
-     
-                
+        
+        // use the same seed, you get the same "random" number!
+
+        // 10-50?
+        int rand2 = rand.nextInt(41)+10;
+                               // 0-40 + 10 = 10 - 50
+        System.out.println("Random number 2 = " + rand2);
+
+        // Random chars: ASCII values/table
+        // Generate random uppercase letter: 65-90
+        // need to cast to a char
+
+        int rand3 = rand.nextInt(26) + 65;
+        System.out.println("Random number 3 = " + rand3);
+        char randomChar = (char)rand3;
+        System.out.println("Random character = " + randomChar);
        
+        sc.close();
   
     }
 }
